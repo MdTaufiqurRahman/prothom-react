221d06d3b07f0ed06f701e5cd0857afead7583e7
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2021-03-12 23:49:40 +0600

    core concept

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const nayoks = ['Jasim', 'Alam', 'Hero', 'Bapparaz'];
+  const nayoks = ['Jasim', 'Alam', 'Hero', 'Bapparaz', 'Bappi'];
+  const friends= ['Jasim', 'Alam', 'Hero', 'Bapparaz', 'Bappi'];
   const products = [
     {name : 'Photoshop', price : '$ 500'},
     {name : 'PdF Reader', price : '$ 50'}
@@ -13,9 +14,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>React Practice</h2>
+        <Counter></Counter>
+        <Users></Users>
 
-        <Product  product = {products[0]} ></Product>
-        <Product  product = {products[1]} ></Product>
+        <ul>{
+          nayoks.map(nayok => <li> {nayok} </li> )
+        }
+        {
+        products.map(product => <li>{product.name}</li> )
+        }
+        </ul>
+        {
+        products.map(pd => <Product product = {pd}></Product>)
+        }
+
+        {
+          friends.map(fnd => <li >{fnd}</li>)
+        }
         
         <Player name = 'Sakib Al hasan' country = 'Bangladesh' ></Player>
         <Player name = 'Virat Koheli' country = 'India'></Player>
@@ -24,6 +39,31 @@ function App() {
       </header>
     </div>
   );
+}
+
+
+function Users (){
+  const [users, setUsers] = useState ([])
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h3>User : {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+          
+        }
+        {
+          users.map(user => <li>{user.phone}</li>)
+        }
+
+      </ul>
+    </div>
+  )
 }
 
 function Product(props){
@@ -56,6 +96,18 @@ function Player(props){
       <h3>Country : {props.country}</h3>
       <button style = {{backgroundColor : 'black', width : '100px', height: '30px', color:'white', border: '1px solid yellow', borderRadius : '5px'}} >Submit</button>
   </div>);
+}
+
+function Counter(){
+  const [count, setCount] =useState(10);
+  const handleClick = () => setCount(count+1);
+  return (
+    <div>
+      <h1>count: {count} </h1>
+      <button onClick={() => setCount(count+1)} >Increase</button>
+      <button onClick={() => setCount(count-1)}>Decrease </button>
+    </div>
+  )
 }
 
 export default App;
